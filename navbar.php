0de751a7105f2c9e67ac3dfa8ebb1ca7846ba48d
jfantab <johnlu1161@gmail.com>
2019-11-21 15:29:53 -0800

    Make sure navbar.php redirects to main.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-expand-sm">
     <div class="container-fluid">
-    <a class="navbar-brand" href="main.html" id="logo"><h3>Peas.com</h3></a>
+    <a class="navbar-brand" href="main.php" id="logo"><h3>Peas.com</h3></a>
     <ul class="navbar-nav" id="links">
         <li class="nav-item item">
             <a href="user.html" class="text-dark btn"><h3>User</h3></a>

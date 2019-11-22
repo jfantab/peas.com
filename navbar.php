92f99f5f78c381a2331cbed07ce777c2378794f7
m-vandertuig <31710577+m-vandertuig@users.noreply.github.com>
2019-11-22 10:36:26 -0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand" href="main.php" id="logo"><h3>Peas.com</h3></a>
     <ul class="navbar-nav" id="links">
         <li class="nav-item item">
-            <a href="user.html" class="text-dark btn"><h3>User</h3></a>
+            <a href="user.php" class="text-dark btn"><h3>User</h3></a>
         </li>
         <li class="nav-item item">
             <a href="loginSystem/login.php" class="text-dark btn"><h3>Login/Register</h3></a>

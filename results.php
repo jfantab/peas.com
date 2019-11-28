3303c6a59d65531442db4489f9fa023981144cb3
WillKolada <57021938+WillKolada@users.noreply.github.com>
2019-11-27 16:20:34 -0800

    Implemented update function structure

--- results.php
+++ results.php
@@ -10,6 +10,51 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
+    <!-- Functions-->
+    <?php
+    $recipes = array(
+        $recipeIDs = array();
+        $recipeImages = array();
+        $recipeServings = array();
+        $recipeLikes = array();
+        $recipePrepTime = array();
+        $recipeName = array();
+        $recipeInstructions = array();
+        $recipeIsDairyFree = array();
+        $recipeIsVegan = array();
+        $recipeIsLowFODMAP = array();
+        $recipeIsGlutenFree = array();
+    );
+    function updateArrays() {
+        for ($i = 0; $i < count($recipes); $i++) {
+            for ($j = 0; $j < count($recipes[$i]); $j++) {
+                if ($j == 0) {
+
+                } elseif ($j == 1) {
+
+                } elseif ($j == 2) {
+
+                } elseif ($j == 3) {
+
+                } elseif ($j == 4) {
+
+                } elseif ($j == 5) {
+
+                } elseif ($j == 6) {
+
+                } elseif ($j == 7) {
+
+                } elseif ($j == 8) {
+
+                } elseif ($j == 9) {
+
+                } elseif ($j == 10) {
+
+                }
+            }
+        }
+    }
+
     <title>Search Results</title>
 </head>
 <body>

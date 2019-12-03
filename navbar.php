ccb2ee21339d2e58b6c064478a47a27b5084e00a
m-vandertuig <31710577+m-vandertuig@users.noreply.github.com>
2019-12-02 18:05:47 -0800

    Added Hidden User Link

--- navbar.php
+++ navbar.php
@@ -5,19 +5,23 @@
      if (!isset($_SESSION['username'])) {
         $buttonText = "Login/Register";
         $link = "loginSystem/login.php";
+        $user = False;
       }
      else{
         $buttonText = "Log Out";
          $link = "loginSystem/logout.php";
+         $user = True;
      }
 ?>
 <div class="navbar navbar-expand-sm">
     <div class="container-fluid">
     <a class="navbar-brand" href="main.php" id="logo"><h3>Peas.com</h3></a>
     <ul class="navbar-nav" id="links">
-        <li class="nav-item item">
-            <a href="user.php" class="text-dark btn"><h3>User</h3></a>
-        </li>
+        <?php if($user) { ?>
+            <li class="nav-item item">
+                <a href="user.php" class="text-dark btn"><h3>User</h3></a>
+            </li>
+        <?php } ?>
         <li class="nav-item item">
             <a href=<?php echo $link; ?> class="text-dark btn"><h3><?php echo $buttonText; ?></h3></a>
         </li>

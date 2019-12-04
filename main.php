0d009cb812cdb315e361534730d8996d92bdf8ee
HayLew650 <hfl127@hotmail.com>
2019-12-03 16:07:43 -0800

    Testing

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     $namePlaceholder = "";
     if(!isset($_SESSION['username'])){
         $namePlaceholder = "";
--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    //session_start();
     $buttonText = "";
     $link = "";
      if (!isset($_SESSION['username'])) {

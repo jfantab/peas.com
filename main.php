a31ceb7aeb757d8e25240f726a8b2a0ea001b971
jfantab <johnlu1161@gmail.com>
2019-12-03 19:22:39 -0800

    Update main.php

--- main.php
+++ main.php
@@ -44,27 +44,25 @@
                     <div class="input-group">
                         <input class="form-control" type="text" placeholder="Search for recipes by ingredients" name="ingredient_input" id="ingredients">
                         <span class="input-group-btn">
-                            <button type="button" class="btn btn-primary"><a href="results.php">Submit</a></button>
+                            <input type="button" class="btn btn-primary"><a href="results.php">Submit</a></input>
                         </span>
                     </div>
                     <small>Please enter ingredients separated by commas.</small>
                 </form>
                 <hr>
-
-                <form name=‘fr’ class="formSearch" action="results.php" method=‘POST’>
+                <form name="fr" class="formSearch" action="results.php" method=‘POST’>
                     <div class="input-group">
-
-
                         <input class="form-control" type="search" placeholder="Search for recipes" name="input-search" id="input-search" value="">
                             <?php
-                            if(isset($_REQUEST["input-search"]))
-                                {$recipe_input = $_REQUEST["input-search"];}
+                            if(isset($_REQUEST["input-search"])) {
+                                $recipe_input = $_REQUEST["input-search"];
+                            }
                             ?>
                             <input type="hidden" value="<?php echo $recipe_input ?>">
                             <span><input type="submit"></span>
 
-                        <script type=‘text/javascript’>
-                        document.fr.submit();
+                        <script type="text/javascript">
+                            document.fr.submit();
                         </script>
                     </div>
                 </form>
